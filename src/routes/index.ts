a91a91a6c851c3e0b44d6a62d932cbb50f2a77c6
Sharon Ormachea <ormachea.sharon@gmail.com>
2023-10-09 01:58:12 -0300

    update swagger doc

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,7 +6,28 @@ import user from "./user.routes";
 const routes = Router();
 
 routes.use("/movies", movie);
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *    description: Trae todos los usuarios registrados
+ *    responses:
+ *     '200':
+ *       description: Ok
+ */
 
+/**
+ * @swagger
+ * /user/{email}:
+ *   get:
+ *    description: Trae un usuario por email
+ *    parameters:
+ *     - in: path
+ *       name: email
+ *    responses:
+ *     '200':
+ *       description: Usuario obtenido correctamente
+ */
 routes.use("/user", user);
 
 export default routes;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,15 +2,7 @@ import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
 
 const router = Router();
-/**
- * @swagger
- * /:
- *   get:
- *    description: Trae todos los usuarios registrados
- *    responses:
- *     '200':
- *     description: Ok
- */
+
 router.get("/", UserController.getAll);
 
 router.post("/", UserController.createUser);
