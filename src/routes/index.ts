0ae89c9cee3ebf21f134468fd5929dce2a03455e
Sharon Ormachea <ormachea.sharon@gmail.com>
2023-10-11 02:12:23 -0300

    update swagger post movie

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,80 @@ import { Router } from "express";
 import movie from "./movies.routes";
 import user from "./user.routes";
 
-
 const routes = Router();
+//---------------MOVIES-----------------------------------
+/**
+* @swagger
+* /movies:
+*   get:
+*    description: Trae todas las peliculas registradas
+*    responses:
+*     '200':
+*       description: Ok
+*/
+
+/**
+ * @swagger
+ * /movies:
+ *   post:
+ *     description: Registrar una pelicula
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: "body"
+ *         in: body
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties: {
+ *             titulo: {
+ *               type: string,
+ *               description: La Monja
+ *             },
+ *             descripcion: {
+ *               type: string,
+ *               description: Una pelicula de terror que no da tanto terror pero podria dar más terror si tuviera algo de terror
+ *             },
+ *             portada: {
+ *               type: string,
+ *               description: https://2.bp.blogspot.com/-qeQ2Kz17suc/W5otZQLcDlI/AAAAAAAAVmI/S8CTqeWDBGkJoDBI9KFIM_mLdXfoJDSxACLcBGAs/s0000/la-monja-poster.jpg
+ *             },
+ *             trailer: {
+ *               type: string,
+ *               description: Link del video promocional
+ *             },
+ *             clasificacion:{
+ *               type: string,
+ *               description: +15
+ *             },
+ *             genero: {
+ *               type: string,
+ *               description: femenino
+ *             },
+ *             duracion: {
+ *               type: string,
+ *               description: 120 minutos
+ *             },
+ *             elenco: {
+ *               type: string,
+ *               description: Un elenco cualquiera porque parece que cualquiera puede ser monja y frances
+ *             },
+ *             director: {
+ *               type: string,
+ *               description: Un director de aquellos porque le hiria mejor como panadero que como director
+ *             }
+ *           }
+ *     responses:
+ *       '200': 
+ *         description: Pelicula creada correctamente
+ *         content:
+ *           application/json:
+ *           type: object
+ */
 
 routes.use("/movies", movie);
+//-------------USER----------------------------
+
 /**
  * @swagger
  * /user:
