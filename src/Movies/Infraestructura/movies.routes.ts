f3f3dbe95342986a473dd16ffe9a361d65c598fe
Sharon Ormachea <103262805+SharonOrmachea@users.noreply.github.com>
2023-12-05 16:37:12 -0300

    update movies controller and routes

--- src/Movies/Infraestructura/movies.routes.ts
+++ src/Movies/Infraestructura/movies.routes.ts
@@ -4,7 +4,13 @@ import { MovieController } from "./movies.controller";
 const router = Router();
 
 router.get("/", MovieController.getAll);
+//falta get por id
+router.get("/:id", MovieController.getById);
 
 router.post("/", MovieController.addMovie);
+//falta put por id
+router.put("/:id", MovieController.updateMovie);
+
+router.delete("/:id", MovieController.deleteMovie);
 
 export default router;
